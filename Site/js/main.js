0bf1ebab663ef52aa85f29cb1f6eae5706440b17
Jeanna <jawa@fullsail.edu>
2014-03-13 16:32:36 -0400

    non functioning progress bar

--- Site/js/main.js
+++ Site/js/main.js
@@ -1,10 +1,29 @@
 //main.js
 
-$(document).ready(function(){
-	$('#playBtn').on('click',function()
+var flashReady = Function(){
+
+	$('#playBtn').on('click',function(){
+		flash.connect('rtmp://localhost');
+	});
 
 	$( "#stop" ).mouseover(function() {
   		$(this).css("background-color", "blue");
 	});
+
+	mediaPlayer.addEventListener('timeupdate', updateProgressBar, false);
+	
+};
+
+
+
+	
+
+	function updateProgressBar() {
+	   var progressBar = $('#progress-bar');
+	   var percentage = Math.floor((100 / mediaPlayer.duration) *
+	   	mediaPlayer.currentTime);
+	   progressBar.value = percentage;
+	   progressBar.innerHTML = percentage + '% played';
+}
 
 });
