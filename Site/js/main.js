976c6ac978a37ede39da8e88601c16614a3ebd31
Jeanna <jawa@fullsail.edu>
2014-03-13 21:53:07 -0400

    lab 3 video link

--- Site/js/main.js
+++ Site/js/main.js
@@ -1,10 +1,28 @@
 //main.js
 
-var flashReady = Function(){
-
-	$('#playBtn').on('click',function(){
-		flash.connect('rtmp://localhost');
+$(document).ready(function(){
+	$('#play').on('click',function(){
+		console.log('play_clicked');
+		
 	});
+
+	$('#stop').on('click',function(){
+		console.log('stop_clicked');
+	});
+
+	$('#volume').on('click',function(){
+		console.log('volume_clicked');
+	});
+
+	$('#camera').on('click',function(){
+		console.log('camera_clicked');
+	});
+
+	$('#record').on('click',function(){
+		console.log('record_clicked');
+	});
+
+var flashReady = function(){
 
 	$( "#stop" ).mouseover(function() {
   		$(this).css("background-color", "blue");
@@ -14,16 +32,12 @@ var flashReady = Function(){
 	
 };
 
-
-
-	
-
 	function updateProgressBar() {
 	   var progressBar = $('#progress-bar');
 	   var percentage = Math.floor((100 / mediaPlayer.duration) *
 	   	mediaPlayer.currentTime);
 	   progressBar.value = percentage;
 	   progressBar.innerHTML = percentage + '% played';
-}
+	};
 
 });
