d76cd3a2f2eaf4e69d2e49bb1e07c0a72deb8fd9
Jeanna <jawa@fullsail.edu>
2014-03-18 23:56:21 -0400

    updating
    volume changes working
    play pause not working

--- Site/js/main.js
+++ Site/js/main.js
@@ -1,39 +1,116 @@
 //main.js
 //
 var play = "";
+var setVol = -1;
+var cams = [];
+var time = "";
+
+var camera = [];
+var mic = [];
+
+var filename = "";
+var recorder = 0;
+
 
 var flashReady = function(){
-	$('#stop').on('click',function(){
+	$('#stop').on('click',function(e){
+		e.preventDefault();
 		console.log('stop_clicked');
+		flash.stopPlaying();
+	});
+
+	$('#play').on('click',function(e){
+		e.preventDefault();
+		console.log('play_clicked');
+		play = 1;
+		flash.connect('rtmp:/SMSServer');
+		$('#play').replaceWith('<a  id="pause" href="#"><img src="img/pause.svg"></a>');
+
+	});
+
+	$('#pause').on('click',function(e){
+		e.preventDefault();
+		console.log('pause_clicked');
+		//play = 0;
+		
+		$('#pause').replaceWith('<a id="play" href="#"><img  src="img/play.svg"></a>');
 		flash.playPause();
 	});
-
-	$('#play').on('click',function(){
-		console.log('play_clicked');
-		flash.connect('rtmp:/SMSServer');
-		play = true;
-	});
-
-	$('#volume').on('click',function(){
+	
+	$('#rightControls').on('click','#volume',function(e){
 		console.log('volume_clicked');
+		var vol = flash.getVolume();
+		e.preventDefault();
+		// console.log(vol);
+
+		//change the setVol everytime clicked
+		setVol += 1;
+
+		//swap volume image depending on volume level
+		if(setVol == 0){
+			$('#volume img').replaceWith('<img src="img/lowVol.svg">');	
+		}
+		else if(setVol == 1){
+			$('#volume img').replaceWith('<img src="img/twoVol.svg">');	
+		}
+		else if(setVol == 2){
+			$('#volume img').replaceWith('<img src="img/threeVol.svg">');	
+		}
+		else if(setVol == 3){ 			
+			$('#volume img').replaceWith('<img src="img/volume.svg">');	
+		}
+			
+		if(setVol > 3){
+			setVol = -1;
+		}
+
+		flash.setVolume(setVol);
+		console.log('setvol ' + setVol);
 	});
 
-	$('#camera').on('click',function(){
+
+	$('#camera').on('click',function(e){
 		console.log('camera_clicked');
+		e.preventDefault();
 
-		cams = [];
-
-		flash.getCameras();
+		cameras = flash.getCameras();
 
 		cams = cameras;
 
-		console.log(cams);
+		console.log(cameras);
 	});
 
-	$('#record').on('click',function(){
-		console.log('record_clicked');
+	$('#mic').on('click',function(e){
+		e.preventDefault();
+		console.log('mic_clicked');
+		microphones = flash.getMicrophones();
+		console.log(microphones);
 
-		flash.startRecording()
+		mic = microphones[0];
+	});
+
+
+	$('#record').on('click',function(e){
+		e.preventDefault();
+		console.log('record_clicked');
+		
+		console.log(mic + cams);
+		record = 1;
+		flash.connect('rtmp:/SMSServer');
+		$('#record').replaceWith('<a id="recording" href="#"><img src="img/recording.svg"></a>');
+
+	});
+
+	$('#rightControls').on('click','#recording',function(e){
+		e.preventDefault();
+		console.log('recording_clicked');
+		
+		console.log(mic + cams);
+
+		record = 0;
+		flash.stopRecording();
+		$('#recording').replaceWith('<a id="record" href="#"><img src="img/record.svg"></a>');
+
 	});
 };
 
@@ -42,10 +119,17 @@ var connected = function(success,error){
 			
 		console.log('success Im running');
 		
-		if(play = true){
+		if(play = 1){
 			flash.startPlaying('startrekintodarkness_vp6.flv');
-			
+			//$('#play').replaceWith('<img src="img/pause.svg">');
+			//flash.playPause();
 		}
+
+		// if(record = 1){
+		// 	flash.startRecording("recording.flv",cams[0], mic[0]);
+		// }
+
+
 	}
 
 	if(success == false){
@@ -54,11 +138,15 @@ var connected = function(success,error){
 	}
 };
 
+var seekTime = function(time){
+
+}
+
 var recordingError = function(message,code){
 	console.log('somethings up with recording ' + message);
 	console.log('code for recording error ' + code)
 }
 
 var globalError = function(message){
-	console.log('what went wrong???' + message);
+	console.log('what went wrong??? ' + message);
 };
